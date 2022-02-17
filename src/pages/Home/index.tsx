731f9db85fb181b6f97d440f818ed00948165f26
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-17 10:03:32 -0300

    feat(home): criando pagina de home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 
+import { Button } from '../../components/Button';
+
 export function Home() {  
 	const navigate = useNavigate();
   const { user, logout } = useAuth();
@@ -10,12 +12,27 @@ export function Home() {
     navigate('/');
   }
   return (
-    <>
-      <h1>Olá {user?.name}</h1>
-      {user?.google && (
-        <span> você está logado com Google</span>
+    <div className="py-24 px-36 flex flex-col gap-24">
+      <section className="flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <h1 className="text-3xl font-bold">Olá, {user?.name.split(' ')[0]}</h1>
+          <span>
+            você acabou de logar com
+            <strong>{user?.google ? 'Google' : ''}</strong>
+          </span>
+        </div>
+        <button onClick={handleLogout}>Sair daqui</button>
+      </section>
+      {user && (
+        <section className="p-10 flex justify-start gap-8 bg-gray-eli-2 rounded-md">
+          <img className="rounded-full w-32 h-32" src={user.avatar} alt="" />
+          <div className="flex flex-col gap-2">
+            <h3 className="text-xl font-bold">Meu perfil</h3>
+            <span>Trexto inútil só pra não focar vazio</span>
+            <Button>VISUALIZAR PERFIL</Button>
+          </div>
+        </section>
       )}
-      <button onClick={handleLogout}>Sair daqui</button>
-    </>
+    </div>
   );
 }
