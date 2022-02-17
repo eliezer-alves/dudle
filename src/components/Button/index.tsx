0c108b3fe1c948ef377261e00b4a4d0ba94e46f7
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-17 00:38:18 -0300

    feat(colors): definindo paleta de cores purple

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 export function Button({ ...props }: ButtonProps) {
 	return (
 		<button
-			className='w-full py-4 px-6 flex items-center justify-center gap-2 bg-gray-eli-1 rounded-md text-xl font-bold hover:bg-purple-eli-3'
+			className='w-full py-4 px-6 flex items-center justify-center gap-2 bg-gray-eli-1 rounded-md text-xl font-bold hover:bg-purple-eli-4'
 			{...props}
 		></button>
 	)
