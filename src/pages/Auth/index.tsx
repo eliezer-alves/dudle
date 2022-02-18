beb6676da6e9d99c0274a952ddf00fd798aa47a4
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-17 21:05:21 -0300

    refact: normalizando limitadores dos argumentos da propriedade className para aspas duplas

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -28,14 +28,14 @@ export function Login() {
   }
 
   return (
-    <div className='flex flex-row h-full p-24'>      
-      <aside className='w-1/2 flex flex-col gap-12 items-center justify-center p-10'>
+    <div className="flex flex-row h-full p-24">      
+      <aside className="w-1/2 flex flex-col gap-12 items-center justify-center p-10">
         <img src={illustrationImg} alt="Ilustração ilustrativa" />
-        <h1 className='text-3xl font-bold text-center px-10'>Faça seu login na plataforma</h1>
+        <h1 className="text-3xl font-bold text-center px-10">Faça seu login na plataforma</h1>
       </aside>
-      <main className='w-1/2 flex items-center justify-center'>
-        <div className='w-2/3 min-h-1/3 gap-2 flex flex-col items-center justify-center p-10 bg-gray-eli-2 rounded-md'>
-          <span className='pb-4 text-purple-eli-4 font-bold'>ENTRAR COM</span>
+      <main className="w-1/2 flex items-center justify-center">
+        <div className="w-2/3 min-h-1/3 gap-2 flex flex-col items-center justify-center p-10 bg-gray-eli-2 rounded-md">
+          <span className="pb-4 text-purple-eli-4 font-bold">ENTRAR COM</span>
           <Button onClick={handleLoginWithGoogle}>
             <img width="20px" src={googleIcon} alt="Sign in with Google"/>
             Google
