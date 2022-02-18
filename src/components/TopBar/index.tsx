feed4b99e143dc07af7e183c1bbfaf33866b7fc6
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-18 09:37:15 -0300

    feat(navigation): criando esqueleto dos links de navegacao

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -16,6 +16,13 @@ export function TopBar() {
   return (
     <div className="w-full h-62 flex justify-between items-center bg-gray-eli-2 py-6 px-24">
       <Logo/>
+      <div>
+        <ul style={{fontFamily: 'Orbitron'}} className="flex gap-8">
+          <li className="link-eli-2">page 1</li>
+          <li className="link-eli-2">page 2</li>
+          <li className="link-eli-2">page 3</li>
+        </ul>
+      </div>
       <img onClick={handleLogout} src={logoutIcon} alt="Sair da plataforma" className="link-eli-2"/>
     </div>
   );
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -2,9 +2,10 @@ import googleIcon from './images/google-icon.svg';
 import githubIcon from './images/github-icon.svg';
 import illustrationImg from './images/developer.svg';
 
-import { useAuth } from '../../hooks/useAuth';
-import { Button } from '../../components/Button';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { Logo } from '../../components/Logo';
+import { Button } from '../../components/Button';
 
 export function Login() {
 	const navigate = useNavigate();
@@ -30,10 +31,11 @@ export function Login() {
   return (
     <div className="flex flex-row h-full p-24">      
       <aside className="w-1/2 flex flex-col gap-12 items-center justify-center p-10">
-        <img src={illustrationImg} alt="Ilustração ilustrativa" />
+        <img src={illustrationImg} alt="Ilustração ilustrativa" />        
         <h1 className="text-3xl font-bold text-center px-10">Faça seu login na plataforma</h1>
       </aside>
-      <main className="w-1/2 flex items-center justify-center">
+      <main className="w-1/2 flex flex-col gap-12 items-center justify-center">
+        <Logo />
         <div className="w-2/3 min-h-1/3 gap-2 flex flex-col items-center justify-center p-10 bg-gray-eli-2 rounded-md">
           <span className="pb-4 text-purple-eli-4 font-bold">ENTRAR COM</span>
           <Button onClick={handleLoginWithGoogle}>
