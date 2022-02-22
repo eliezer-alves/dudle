3e63a54c23af1e335ab5095935f750eb7d74ffa8
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-22 09:18:51 -0300

    feat(home): exibindo todos os posts na pagina home

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -16,11 +16,15 @@ export function Post({ id, title, post, author, createdAt, children }: PostProps
   console.log(createdAt);
   
   return (
-    <div className="mt-4 p-10 flex flex-col gap-4 bg-gray-eli-2 rounded-md">
-      <div className="flex justify-between">
-        <h1 className="text-2xl font-bold">{title}</h1>
+    <div className="mt-4 p-10 pt-5 flex flex-col gap-4 bg-gray-eli-2 rounded-md">
+      <div className="mb-5 flex justify-between items-end">
+        <span className="flex items-end gap-4">
+          <img src={author.avatar} alt="Author of the post" className="rounded-full w-12 h-12"/>
+          {author.name}
+        </span>
         <span>{createdAt}</span>
       </div>
+      <h1 className="text-2xl font-bold">{title}</h1>
       <span>{post}</span>
     </div>
   )
